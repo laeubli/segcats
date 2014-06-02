ca025eb5b01fc0dceaea020134668d6c65956395
laeubli <s.laeubli@me.com>
2014-06-02 21:46:55 +0100

    refactor Forward and Backward probability implementation

--- testDriver.py
+++ testDriver.py
@@ -19,12 +19,22 @@ myHMM = model.HMM(states=states, features=features, observation_sequences=observ
 
 test_obs_seq = [ ['keydown', 31.0, 'source'], ['keydown', 100.0, 'source'], ['fixation', 200.0, 'target'], ]
 
+# FORWARD
+print "Forward probability tests"
+
+print myHMM.forwardTrellis( test_obs_seq )
 print myHMM.forwardProbability( test_obs_seq ) # forward probability of the whole observation sequence (=total observation probability)
 print myHMM.forwardProbability( test_obs_seq, 0 ) # forward probabilities for all states at time 0
 print myHMM.forwardProbability( test_obs_seq, 1, 'H3' ) # forward probability for state H3 at time 1
 
+# BACKWARD
+print "\nBackward probability tests"
+
+print myHMM.backwardTrellis( test_obs_seq )
 print myHMM.backwardProbability( test_obs_seq ) # backward probability of the whole observation sequence (=total observation probability)
 print myHMM.backwardProbability( test_obs_seq, 0 ) # backward probabilities for all states at time 0
 print myHMM.backwardProbability( test_obs_seq, 1, 'H3' ) # backward probability for state H3 at time 1
 
+# VITERBI
+print "\nViterbi tests"
 print myHMM.viterbiProbability( test_obs_seq )
