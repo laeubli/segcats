d3de8434accb3af7675df19c382cc82858d4c07f
laeubli <s.laeubli@me.com>
2014-06-23 23:49:17 +0100

    init visualise package

---
+++ visualise/visualise.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Statistical Modelling of Human Translation Processes
+# Author: Samuel Läubli (slaubli@inf.ed.ac.uk)
+
+"""
+Provides various functions to plot data through matplotlib. All plots can either
+be displayed in a pop-up window during runtime or saved to file.
+"""
+
+from __future__ import division
+
