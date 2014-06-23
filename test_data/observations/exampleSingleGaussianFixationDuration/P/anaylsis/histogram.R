d8d1e61ff55b1c962730f0c1e9eb919fac54cc76
laeubli <s.laeubli@me.com>
2014-06-23 23:52:27 +0100

    remove test_data/ from this repository

--- test_data/observations/exampleSingleGaussianFixationDuration/P/anaylsis/histogram.R
+++
@@ -1,8 +0,0 @@
-setwd('/Users/sam/Documents/ausbildung/uni/msc_ai/thesis/Code/segcats/test_data/observations/exampleSingleGaussianFixationDuration/P/anaylsis')
-data.P = read.csv('all-P.obs')
-
-hist(data.P$fixation_dur)
-hist(data.P$fixation_dur[data.P$fixation_dur < 1000])
-hist(data.P$fixation_dur[data.P$fixation_dur < 2000])
-#hist(data.P$fixation_dur[data.P$fixation_dur < 500])
-length(data.P$fixation_dur)
