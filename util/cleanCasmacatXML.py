9fdf1d235e2380de856ed01eeed7d44f05dd6128
laeubli <s.laeubli@me.com>
2014-06-24 10:00:31 +0100

    init util package

---
+++ util/cleanCasmacatXML.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Statistical Modelling of Human Trnaslation Processes
+# Author: Samuel Läubli (slaubli@inf.ed.ac.uk)
+
+"""
+Analysis and cleaning tool for potentially error-prone CASMACAT XML files.
+"""
+
+from __future__ import division
+
